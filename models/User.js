bb541590da23ff9674370ff43c034dff712bfa09
Ronny599 <102850680+Ronny599@users.noreply.github.com>
2023-09-23 20:08:40 +0530

    1 file added

---
+++ models/User.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const blogSchema = new Schema({
+  name:{
+    type: String,
+    required: true
+  },
+  email:{
+    type: String,
+    required: true
+  },
+  password:{
+    type: String,
+    required: true
+  },
+  date:{
+    type: Date,
+    default: Date.now,
+    required: true
+  },
+});
