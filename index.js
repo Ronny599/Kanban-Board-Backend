4c002216134de916a37ef136f6893b76a74e1c43
Ronny599 <102850680+Ronny599@users.noreply.github.com>
2023-09-24 20:18:24 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const port = 3010;
 const path = require('path');
-const User= require('../models/User');
+const User= require('./models/User');
 
 // Use of Middleware
 // app.use(express.json); 
